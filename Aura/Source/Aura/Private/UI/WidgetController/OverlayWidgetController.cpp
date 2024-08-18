f5b65ccd5c48b4924a0749bb9d67a64809e41151
Chen <1360950848@qq.com>
2024-08-18 23:34:04 +0800

    spell widget

--- Aura/Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Aura/Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -5,6 +5,7 @@
 
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AbilityInfo.h"
 
 void UOverlayWidgetController::BroadcastInitalValues()
 {
@@ -41,22 +42,47 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 		                      OnMaxManaChanged.Broadcast(Data.NewValue);
 	                      });
 
-	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
-		[this](const FGameplayTagContainer& AssetTags)
+
+	if (UAuraAbilitySystemComponent* AuraASC = Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent))
+	{
+		if (AuraASC->IsStartupAbilitiesGiven())
 		{
-			for (const FGameplayTag& Tag : AssetTags)
-			{
-				//"A.1".MatchesTag("A") will return true, "A".MatchesTag("A.1") will return false
-				FGameplayTag Message = FGameplayTag::RequestGameplayTag("Message");
-				if (Tag.MatchesTag(Message))
-				{
-					FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable, Tag);
-					MessageWidgetRowSignature.Broadcast(*Row);
-				}
-				/*const FString Msg = FString::Printf(TEXT("%s"), *Tag.ToString());
-				GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);*/
-			}
+			OnInitializeStartupAbilities(AuraASC);
 		}
-	);
+		else
+		{
+			AuraASC->AbilitiesGivenDelegate.AddUObject(this, &UOverlayWidgetController::OnInitializeStartupAbilities);
+		}
+
+		AuraASC->EffectAssetTags.AddLambda(
+			[this](const FGameplayTagContainer& AssetTags)
+			{
+				for (const FGameplayTag& Tag : AssetTags)
+				{
+					//"A.1".MatchesTag("A") will return true, "A".MatchesTag("A.1") will return false
+					FGameplayTag Message = FGameplayTag::RequestGameplayTag("Message");
+					if (Tag.MatchesTag(Message))
+					{
+						FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable, Tag);
+						MessageWidgetRowSignature.Broadcast(*Row);
+					}
+					/*const FString Msg = FString::Printf(TEXT("%s"), *Tag.ToString());
+					GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);*/
+				}
+			}
+		);
+	}
 }
 
+void UOverlayWidgetController::OnInitializeStartupAbilities(UAuraAbilitySystemComponent* AuraASC)
+{
+	if (!AuraASC->IsStartupAbilitiesGiven()) { return; }
+	FForEachAbility BroadcastDelegate;
+	BroadcastDelegate.BindLambda([this,AuraASC](const FGameplayAbilitySpec& Spec)
+	{
+		FAuraAbilityInfo Info = AbilityInfo->FindAbilityInfoByTag(AuraASC->GetAbilityTagFromSpec(Spec));
+		Info.InputTag = AuraASC->GetInputTagFromSpec(Spec);
+		AbilityInfoDelegate.Broadcast(Info);
+	});
+	AuraASC->ForEachAbility(BroadcastDelegate);
+}
--- Aura/Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Aura/Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -8,6 +8,10 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "OverlayWidgetController.generated.h"
 
+struct FAuraAbilityInfo;
+class UAuraAbilitySystemComponent;
+class UAbilityInfo;
+
 USTRUCT()
 struct FUIWidgetRow : public FTableRowBase
 {
@@ -28,6 +32,7 @@ struct FUIWidgetRow : public FTableRowBase
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAttributeDataChangedSignature, float, NewData);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageWidgetRowSignature, FUIWidgetRow, Row);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAbilityInfoSignature, const FAuraAbilityInfo&, Info);
 /**
  * 
  */
@@ -54,13 +59,21 @@ public:
 
 	UPROPERTY(BlueprintAssignable, Category="GAS|Message")
 	FMessageWidgetRowSignature MessageWidgetRowSignature;
-	
+
+	UPROPERTY(BlueprintAssignable, Category="GAS|Message")
+	FAbilityInfoSignature AbilityInfoDelegate;
 protected:
 	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Widget Data")
 	TObjectPtr<UDataTable> MessageWidgetDataTable;
 
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Widget Data")
+	TObjectPtr<UAbilityInfo> AbilityInfo;
+
 	template <typename T>
 	T* GetDataTableRowByTag(UDataTable* DataTable,const FGameplayTag& Tag);
+
+	UFUNCTION()
+	void OnInitializeStartupAbilities(UAuraAbilitySystemComponent* AuraASC);
 };
 
 template <typename T>
